ce90b6cde1c20f30e47a482f575ba6bed9ca0ed3
Eduardo <eduardopoox.99@gmail.com>
2021-02-27 14:01:53 -0600

    Primer commit del proyecto

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="https://github.com/EduardoPoox" target="_blank">EduardoPoox</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
